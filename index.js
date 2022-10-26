d6ed0a516064130dddc2a1af3047a02a5593972c
Maruf9270 <Maruf9270@gmail.com>
2022-10-27 00:17:55 +0600

    Added image link to the categogies

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app= express();
+const cors = require('cors')
+app.use(cors());
 const Port = process.env.PORT || 3150;
 // Importing the course cateory file from the directory\
 const categories = require('./CouseCategory.json')
