9373e001a782196ddfbd7e8dfdb996ba70ac4a70
Maruf9270 <Maruf9270@gmail.com>
2022-10-26 20:10:34 +0600

    Added course category section

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app= express();
 const Port = process.env.PORT || 3150;
-
+// Importing the course cateory file from the directory\
+const categories = require('./CouseCategory.json')
 
 app.get('/',(req,res)=>{
     res.send("Code with maruf server running")
 })
 app.get('/test', (req,res)=>{
     res.send('Your test server is running')
+})
+// getting request for course category and sending response for the course category
+app.get('/categories',(req,res)=>{
+    res.send(categories)
+    
 })
 
 
